2db47f65a958c26e2649b10c95c80b9241387ce1
bhushan dighde <bhushandighde11@gmail.com>
2026-07-16 11:27:14 +0530

    Bug Fixing deployment

--- Program.cs
+++ Program.cs
@@ -104,11 +104,10 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseSerilogRequestLogging();
 if (!app.Environment.IsProduction())
